1f2ec4349685d6280032ae1f0af488cf23b768fd
theent <33178512+theent@users.noreply.github.com>
2021-05-20 16:36:55 +0200

    La till liten kommentar på factory

--- src/main/java/model/CellFactory.java
+++ src/main/java/model/CellFactory.java
@@ -2,7 +2,16 @@ package model;
 
 public class CellFactory {
 
-
+    /**
+     * cellKind:
+     * T -> TextCell |
+     * C -> CircularCell |
+     * E -> ExprCell |
+     * else EmptyCell
+     * @param cellKind
+     * @param text
+     * @return
+     */
     public Cell cellMaker (String cellKind, String text) {
         if (cellKind.equals("T")){
             return new TextCell(text, text.substring(1));
