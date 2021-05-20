3599c2bfe4315faf2252f4dd9152ee14b920d30d
theent <33178512+theent@users.noreply.github.com>
2021-05-20 15:50:34 +0200

    prov push till Test

--- src/main/java/model/XLModel.java
+++ src/main/java/model/XLModel.java
@@ -13,10 +13,12 @@ public class XLModel implements Environment {
   // String = Adress, typ B3, CellContent är vad addressen innehåller
   private final Map<String, Cell> cells;      //Kvalificerad association
   private final List<OnUpdateObserver> observers;
+  private final List<String> expressions;
 
   public XLModel(){
     cells = new HashMap<>();
     observers = new ArrayList<>();
+    expressions = new ArrayList<>();
 
     for (int i = 0; i < ROWS; i++) {
       for (int j = 0; j < COLUMNS; j++) {
@@ -35,19 +37,26 @@ public class XLModel implements Environment {
     if (text.length() == 0){
       EmptyCell eC = new EmptyCell();
       cells.put(address, eC);
+      expressions.remove(address);
       notifyObservers(address, eC.toString());
     } else if (text.charAt(0) == '#'){
       TextCell tC = new TextCell(text, text.substring(1));
       cells.put(address, tC);
+      expressions.remove(address);
       notifyObservers(address, tC.toString());
     } else{
       evaluateExpr(text, address);
+      expressions.add(address);
     }
 
-    cells.forEach((key, value) -> {
+    for(String adr : expressions) {
+      evaluateExpr(cells.get(adr).inputText(), adr);
+    }
+
+/*    cells.forEach((key, value) -> {
       if (value instanceof ExprCell)
         evaluateExpr(value.inputText(), key);
-    });
+    });*/
   }
 
   /**
@@ -71,6 +80,7 @@ public class XLModel implements Environment {
   public void clearCell(String address){
     Cell c = new EmptyCell();
     cells.put(address, c);
+    expressions.remove(address);
     notifyObservers(address, c.toString());
   }
 
